29200fc22e5af49bd2c3ea5ff98072947cc9e46d
Ezequiel Lopes <ezequiel.jn98@outlook.com>
2024-06-23 23:10:26 -0300

    Refactor main.go to use port 8080 as default

--- main.go
+++ main.go
@@ -3,36 +3,41 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log/slog"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
 	slog.Info("Starting server")
-	healthzFail := false
+	start := time.Now()
 	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		az, err := myAz()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			b, _ := json.Marshal(map[string]string{"message": err.Error()})
+			w.Write(b)
+			return
+		}
 		w.Header().Set("Content-Type", "application/json")
-		b, _ := json.Marshal(map[string]string{"message": "ok"})
+		b, _ := json.Marshal(map[string]string{
+			"message": "ok",
+			"az":      az,
+		})
 		w.Write(b)
 	})
 
 	http.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		if healthzFail {
+		if time.Since(start).Seconds() > 60 {
 			b, _ := json.Marshal(map[string]string{"status": "fail"})
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write(b)
 			return
 		}
 		b, _ := json.Marshal(map[string]string{"status": "ok"})
-		w.Write(b)
-	})
-
-	http.HandleFunc("GET /healthz/toggle", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		healthzFail = !healthzFail
-		b, _ := json.Marshal(map[string]bool{"healthz status": healthzFail})
 		w.Write(b)
 	})
 
@@ -44,4 +49,38 @@ func main() {
 	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil); err != nil {
 		panic(err)
 	}
+}
+
+func myAz() (string, error) {
+	client := &http.Client{Timeout: 10 * time.Second}
+	req, err := http.NewRequest(http.MethodPut, "http://169.254.169.254/latest/api/token", nil)
+	if err != nil {
+		return "", fmt.Errorf("erro ao criar solicitação: %w", err)
+	}
+	req.Header.Set("X-aws-ec2-metadata-token-ttl-seconds", "21600")
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("erro ao obter metadados: %w", err)
+	}
+	defer resp.Body.Close()
+	token, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("erro ao ler resposta: %w", err)
+	}
+	req, err = http.NewRequest(http.MethodGet, "http://169.254.169.254/latest/meta-data/placement/availability-zone", nil)
+	if err != nil {
+		return "", fmt.Errorf("erro ao criar solicitação: %w", err)
+	}
+	req.Header.Set("X-aws-ec2-metadata-token", string(token))
+	resp, err = client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("erro ao obter metadados: %w", err)
+	}
+	defer resp.Body.Close()
+
+	az, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("erro ao ler resposta: %w", err)
+	}
+	return string(az), nil
 }
